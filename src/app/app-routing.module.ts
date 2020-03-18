b6a61bf50c2108d4a69f985e4854f6b7aeb5a466
almgwary <almgwaryfci@gmail.com>
2020-03-18 03:26:50 +0200

    feat: add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductFormComponent } from './product-form/product-form.component';
+import { ProductFormReactiveComponent } from './product-form-reactive/product-form-reactive.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: ProductListComponent },
+  { path: 'add', component: ProductFormReactiveComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
